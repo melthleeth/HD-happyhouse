2cd9e2f78ef3c74e034d72d1c80c00b4979feb75
Hyejin Lee <hichjay@gmail.com>
2020-11-09 17:10:38 +0900

    덮어쓰기

--- happyhouse_spring_seoul_7_leehyejin_jangdeokin_2/src/main/java/com/ssafy/happyhouse/Main.java
+++ happyhouse_spring_seoul_7_leehyejin_jangdeokin_2/src/main/java/com/ssafy/happyhouse/Main.java
@@ -1,10 +1,10 @@
 package com.ssafy.happyhouse;
 
-import com.ssafy.happyhouse.view.HouseInfoView;
+//import com.ssafy.happyhouse.view.HouseInfoView;
 
 // 왜 안되는가
 public class Main {
 	public static void main(String[] args) {
-		new HouseInfoView();
+//		new HouseInfoView();
 	}
 }
--- happyhouse_spring_seoul_7_leehyejin_jangdeokin_2/src/main/java/com/ssafy/happyhouse/controller/HouseController.java
+++ happyhouse_spring_seoul_7_leehyejin_jangdeokin_2/src/main/java/com/ssafy/happyhouse/controller/HouseController.java
@@ -11,8 +11,6 @@ import javax.servlet.http.*;
 import org.json.simple.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import com.ssafy.guestbook.model.MemberDto;
 import com.ssafy.happyhouse.model.*;
 import com.ssafy.happyhouse.model.service.*;
 
@@ -29,15 +27,4 @@ public class HouseController {
 	@Autowired
 	private HouseMapService houseMapService;
 	
-	@ApiOperation(value = "회원목록", notes = "회원의 <big>전체 목록</big>을 반환해 줍니다.")
-	@ApiResponses({
-		@ApiResponse(code = 200, message = "잘됨"),
-		@ApiResponse(code = 404, message = "404"),
-		@ApiResponse(code = 500, message = "500")
-	})
-	@GetMapping(value = "/user", headers = { "Content-type=application/json" })	
-	public List<MemberDto> userList() {
-//	public @ResponseBody List<MemberDto> userList() {
-		return userService.userList();
-	}
 }
