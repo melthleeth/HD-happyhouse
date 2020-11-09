ec894f80c52d70a87ea9c464bfdbe9cc16a5d4b9
Hyejin Lee <hichjay@gmail.com>
2020-11-09 15:49:51 +0900

    housemap service impl 바꿈

--- happyhouse_spring_seoul_7_leehyejin_jangdeokin_2/src/main/java/com/ssafy/happyhouse/model/service/HouseMapServiceImpl.java
+++ happyhouse_spring_seoul_7_leehyejin_jangdeokin_2/src/main/java/com/ssafy/happyhouse/model/service/HouseMapServiceImpl.java
@@ -2,51 +2,46 @@ package com.ssafy.happyhouse.model.service;
 
 import java.util.List;
 
-import com.ssafy.happyhouse.model.HouseDealDto;
-import com.ssafy.happyhouse.model.HouseInfoDto;
-import com.ssafy.happyhouse.model.SidoGugunCodeDto;
-import com.ssafy.happyhouse.model.dao.HouseMapDaoImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.ssafy.happyhouse.model.*;
+import com.ssafy.happyhouse.model.dao.*;
+
+@Service
 public class HouseMapServiceImpl implements HouseMapService {
 	
-	private static HouseMapService houseMapService;
-	
-	private HouseMapServiceImpl() {}
-	
-	public static HouseMapService getHouseMapService() {
-		if(houseMapService == null)
-			houseMapService = new HouseMapServiceImpl();
-		return houseMapService;
-	}
+	@Autowired
+	private HouseMapDao dao;
 
 	@Override
 	public List<SidoGugunCodeDto> getSido() throws Exception {
-		return HouseMapDaoImpl.getHouseMapDao().getSido();
+		return dao.getSido();
 	}
 
 	@Override
 	public List<SidoGugunCodeDto> getGugunInSido(String sido) throws Exception {
-		return HouseMapDaoImpl.getHouseMapDao().getGugunInSido(sido);
+		return dao.getGugunInSido(sido);
 	}
 
 	@Override
 	public List<HouseDealDto> getDongInGugun(String gugun) throws Exception {
-		return HouseMapDaoImpl.getHouseMapDao().getDongInGugun(gugun);
+		return dao.getDongInGugun(gugun);
 	}
 
 	@Override
 	public List<HouseDealDto> getAptInDong(String dong) throws Exception {
-		return HouseMapDaoImpl.getHouseMapDao().getAptInDong(dong);
+		return dao.getAptInDong(dong);
 	}
 
 	@Override
 	public List<HouseDealDto> getDongInfo(String dongName) throws Exception {
-		return HouseMapDaoImpl.getHouseMapDao().getDongInfo(dongName);
+		return dao.getDongInfo(dongName);
 	}
 
 	@Override
 	public List<HouseDealDto> getAptInfo(String aptName) throws Exception {
-		return HouseMapDaoImpl.getHouseMapDao().getAptInfo(aptName);
+		return dao.getAptInfo(aptName);
 	}
 
 }
