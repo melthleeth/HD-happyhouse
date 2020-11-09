80f5930c3555662d36a7e8d2f7b1ae3e04125f14
Hyejin Lee <hichjay@gmail.com>
2020-11-09 14:23:12 +0900

    제발 되어라

---
+++ happyhouse_spring_seoul_7_leehyejin_jangdeokin_2/src/main/java/com/ssafy/happyhouse/model/service/LoginServiceImpl.java
@@ -0,0 +1,19 @@
+package com.ssafy.happyhouse.model.service;
+
+import com.ssafy.happyhouse.model.dao.*;
+import com.ssafy.happyhouse.model.dto.MemberDto;
+
+public class LoginServiceImpl implements LoginService {
+	
+	LoginDao loginDao;
+	
+	public LoginServiceImpl() {
+		loginDao = new LoginDaoImpl();
+	}
+	
+	@Override
+	public MemberDto login(String username, String password) throws Exception{
+		if (username == null || password == null) return null;
+		return loginDao.login(username, password);
+	}
+}
