bdf12ec70cf10dd97f751798efe55a28d73b727b
Hyejin Lee <hichjay@gmail.com>
2020-11-09 17:03:02 +0900

    Revert "모의 테스"
    This reverts commit 0bb6c4e421827f61c812efda1c99371575b856bf.

--- happyhouse_spring_seoul_7_leehyejin_jangdeokin_2/src/main/java/com/ssafy/happyhouse/Main.java
+++ happyhouse_spring_seoul_7_leehyejin_jangdeokin_2/src/main/java/com/ssafy/happyhouse/Main.java
@@ -1,9 +1,9 @@
 package com.ssafy.happyhouse;
 
-//import com.ssafy.happyhouse.view.HouseInfoView;
+import com.ssafy.happyhouse.view.HouseInfoView;
 
 public class Main {
 	public static void main(String[] args) {
-//		new HouseInfoView();
+		new HouseInfoView();
 	}
 }
