611c0bbb5a61a8e9a8fa82ceb4eb0500165ca30d
JANGDUCKIN <77999jno@naver.com>
2020-11-16 22:42:08 +0900

    11.16

--- happyhouse_spring_seoul_7_leehyejin_jangdeokin_2/src/main/java/com/ssafy/happyhouse/controller/PageController.java
+++ happyhouse_spring_seoul_7_leehyejin_jangdeokin_2/src/main/java/com/ssafy/happyhouse/controller/PageController.java
@@ -34,7 +34,7 @@ public class PageController {
 	}
 	@GetMapping(value = "/qna")
 	public String about() {
-		return "qna";
+		return "redirect:/";
 	}
 	@GetMapping(value = "/management")
 	public String management(Model model, HttpSession session, HttpServletResponse response) {
